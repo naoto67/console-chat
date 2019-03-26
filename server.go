8504a2c3471b63b608e15d6f6898d38c5874d131
naoto67 <uver.com67@gmail.com>
2019-03-27 01:59:46 +0900

    add echo function and add ws function

--- server.go
+++ server.go
@@ -11,13 +11,30 @@ var addr = flag.String("addr", "localhost:8080", "http service address")
 
 var upgrader = websocket.Upgrader{}
 
-func echo(w http.ResponseWriter, r *http.Request) {
+func distribute_message(ws *websockets, mt int, ms []byte) {
+	for _, c := range ws.connections {
+		err := c.WriteMessage(mt, ms)
+		if err != nil {
+			log.Println("write error: ", err)
+			continue
+		}
+	}
+}
+
+type websockets struct {
+	connections []*websocket.Conn
+}
+
+func (ws *websockets) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	c, err := upgrader.Upgrade(w, r, nil)
+	ws.connections = append(ws.connections, c)
+	log.Println(ws)
 	if err != nil {
 		log.Println("upgrade: ", err)
 		return
 	}
 	defer c.Close()
+
 	for {
 		mt, message, err := c.ReadMessage()
 		if err != nil {
@@ -25,29 +42,14 @@ func echo(w http.ResponseWriter, r *http.Request) {
 			break
 		}
 		log.Printf("recv: %s", message)
-		err = c.WriteMessage(mt, message)
-		if err != nil {
-			log.Println("write: ", err)
-			break
-		}
+		distribute_message(ws, mt, message)
 	}
 }
 
-type AnyHandler struct {
-	array []string
-}
-
-func (a *AnyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	a.array = append(a.array, "aaa")
-	log.Println(a)
-	log.Printf("%T", a)
-}
-
 func main() {
-	anyHandler := &AnyHandler{}
+	ws := &websockets{}
 	flag.Parse()
 	log.SetFlags(0)
-	http.HandleFunc("/echo", echo)
-	http.Handle("/sample", anyHandler)
+	http.Handle("/ws", ws)
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
